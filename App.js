7220dfa38542b422a046e2d2d26f3e9bcd1fb9fd
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-29 19:07:36 +0200

    Reg

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Registration from './Screens/RegistrationScreen/RegistrationScreen';
 
 export default function App() {
 
-
   return (
     <View style={styles.container}>
-      <Text>Open App to start on your app!</Text>
-      <StatusBar style="auto" />
+      <Routes>
+        <Route path="registration" element={<Registration />} />
+
+        <Text>Open App to start on your app!</Text>
+        <StatusBar style="auto" />
+      </Routes>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
